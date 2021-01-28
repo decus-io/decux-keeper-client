51f7c86028c2a9392297319273e7748bb3019deb
buildtrust <buildtrust@google.com>
2021-01-29 01:02:06 +0800

    complete multisig by go

--- transaction.go
+++ transaction.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
@@ -12,13 +13,6 @@ import (
 )
 
 func main() {
-	txBytes, _ := hex.DecodeString("02000000011da769e44941ab0631f4b488f65df32d5ef204c01c13b95b4b4522a1fd60f5fe0000000000ffffffff01a08601000000000017a9141276148e7c31f4e61eb1f31d0380391092e5e4578700000000")
-
-	tx, err := btcutil.NewTxFromBytes(txBytes)
-	if err != nil {
-		log.Fatalf("decode tx error: %v", err)
-	}
-
 	pk1Bytes, _ := hex.DecodeString("0305fc63bed535ea0e7896625e34b5d65557d95e5db72839070e95a24f84be22b3")
 	pk1, _ := btcutil.NewAddressPubKey(pk1Bytes, &chaincfg.TestNet3Params)
 	pk2Bytes, _ := hex.DecodeString("02b3ff4cd0b6f271fb372e6114f9d91ca53b26a34031f7f181fae2baee1c5ce855")
@@ -37,58 +31,78 @@ func main() {
 		log.Fatalf("new multisig script error: %v", err)
 	}
 
-	sigHashType := txscript.SigHashAll
-	hash, err := txscript.CalcSignatureHash(redeemScript, sigHashType, tx.MsgTx(), 0)
+	redeemHash := btcutil.Hash160(redeemScript)
+	addr, err := btcutil.NewAddressScriptHashFromHash(redeemHash, &chaincfg.TestNet3Params)
 	if err != nil {
-		log.Fatalf("calc signature hash error: %v", err)
+		log.Fatalf("new address script error: %v", err)
 	}
+	fmt.Println("multisig address: " + addr.EncodeAddress())
+	PkScript, err := txscript.PayToAddrScript(addr)
+	if err != nil {
+		log.Fatalf("pay to address script error: %v", err)
+	}
+
+	preHash, _ := chainhash.NewHashFromStr("fef560fda122454b5bb9131cc004f25e2df35df688b4f43106ab4149e469a71d")
+	tx := &wire.MsgTx{
+		Version: wire.TxVersion,
+		TxIn: []*wire.TxIn{
+			{
+				PreviousOutPoint: wire.OutPoint{
+					Hash:  *preHash,
+					Index: 0,
+				},
+			},
+		},
+		TxOut: []*wire.TxOut{
+			{
+				Value:    900000,
+				PkScript: PkScript,
+			},
+		},
+		LockTime: 0,
+	}
+
+	//sigHashType := txscript.SigHashAll
+	//hash, err := txscript.CalcSignatureHash(redeemScript, sigHashType, tx, 0)
+	//if err != nil {
+	//	log.Fatalf("calc signature hash error: %v", err)
+	//}
 
 	wif1, err := btcutil.DecodeWIF("cQ97jdD2aCcb2MuQd7XVo4cPyQ6UH1J6Jz8LcAetghA9CXwDJcxa")
 	if err != nil {
 		log.Fatalf("decode wif error: %v", err)
 	}
-	sig1, err := wif1.PrivKey.Sign(hash)
+	sig1, err := txscript.RawTxInSignature(tx, 0, redeemScript, txscript.SigHashAll, wif1.PrivKey)
 	if err != nil {
-		log.Fatalf("signature hash error: %v", err)
+		log.Fatalf("sign tx error: %v", err)
 	}
-	sig1WithHashType := append(sig1.Serialize(), byte(sigHashType))
 
-	wif2, err := btcutil.DecodeWIF("cQ97jdD2aCcb2MuQd7XVo4cPyQ6UH1J6Jz8LcAetghA9CXwDJcxa")
+	wif2, err := btcutil.DecodeWIF("cS2cvrAc1PJaixn2v6SLXLRJEH8ULErbV12QjXNX6B5UmZN47WwZ")
 	if err != nil {
 		log.Fatalf("decode wif error: %v", err)
 	}
-	sig2, err := wif2.PrivKey.Sign(hash)
+	sig2, err := txscript.RawTxInSignature(tx, 0, redeemScript, txscript.SigHashAll, wif2.PrivKey)
 	if err != nil {
-		log.Fatalf("signature hash error: %v", err)
+		log.Fatalf("sign tx error: %v", err)
 	}
-	sig2WithHashType := append(sig2.Serialize(), byte(sigHashType))
 
-	wif3, err := btcutil.DecodeWIF("cQ97jdD2aCcb2MuQd7XVo4cPyQ6UH1J6Jz8LcAetghA9CXwDJcxa")
+	wif3, err := btcutil.DecodeWIF("cScpHakK8uEbeVJsE1G92FM79ESe93bbWZsTFbcLHThUuSd3X8LK")
 	if err != nil {
 		log.Fatalf("decode wif error: %v", err)
 	}
-	sig3, err := wif3.PrivKey.Sign(hash)
+	sig3, err := txscript.RawTxInSignature(tx, 0, redeemScript, txscript.SigHashAll, wif3.PrivKey)
 	if err != nil {
-		log.Fatalf("signature hash error: %v", err)
+		log.Fatalf("sign tx error: %v", err)
 	}
-	sig3WithHashType := append(sig3.Serialize(), byte(sigHashType))
 
-	sigScript, err := txscript.NewScriptBuilder().AddData(sig1WithHashType).AddData(sig2WithHashType).AddData(sig3WithHashType).AddData(redeemScript).Script()
+	sigScript, err := txscript.NewScriptBuilder().AddOp(txscript.OP_FALSE).AddData(sig1).AddData(sig2).AddData(sig3).AddData(redeemScript).Script()
 	if err != nil {
 		log.Fatalf("new signature script error: %v", err)
 	}
-	tx.MsgTx().TxIn[0].SignatureScript = sigScript
+	tx.TxIn[0].SignatureScript = sigScript
 
-	tx1 := &wire.MsgTx{
-		Version:  wire.TxVersion,
-		TxIn:     tx.MsgTx().TxIn,
-		TxOut:    tx.MsgTx().TxOut,
-		LockTime: 0,
-	}
-
-	fmt.Println(hex.EncodeToString(sigScript))
-	buf := bytes.NewBuffer(make([]byte, 0, tx1.SerializeSize()))
-	if err := tx1.Serialize(buf); err != nil {
+	buf := bytes.NewBuffer(make([]byte, 0, tx.SerializeSize()))
+	if err := tx.Serialize(buf); err != nil {
 		log.Fatalf("serialize tx data error: %v", err)
 	}
 	fmt.Println(hex.EncodeToString(buf.Bytes()))
